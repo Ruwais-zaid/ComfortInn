615449fc9e12ef80e23d93b9859aa7c2655b114f
Ruwais-zaid <zaid.btech2022sitpune.edu.in>
2024-06-18 13:52:38 +0530

    add tsconfig.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,7 +36,7 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/dist/frontend")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
